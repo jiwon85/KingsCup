bf000aa2cfe556150f6b12107b8bfc500ab40ae1
jiwon85 <jiwon.min85@gmail.com>
2015-02-26 11:26:36 +0000

    Initial Project commit.

---
+++ app/src/androidTest/java/com/iphoneteam/jiwon_85/cs4084/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.iphoneteam.jiwon_85.cs4084;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
