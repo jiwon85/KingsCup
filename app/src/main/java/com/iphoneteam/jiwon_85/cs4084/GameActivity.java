96d9f3c2b26a5c7a53308f9195344a09e4a10b54
jiwon85 <jiwon.min85@gmail.com>
2015-02-27 14:47:24 +0000

    Added splash screen skeleton

--- app/src/main/java/com/iphoneteam/jiwon_85/cs4084/GameActivity.java
+++ app/src/main/java/com/iphoneteam/jiwon_85/cs4084/GameActivity.java
@@ -1,42 +1,24 @@
 package com.iphoneteam.jiwon_85.cs4084;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-
-public class MainActivity extends ActionBarActivity {
-
-//    @ViewById
-//    Button startButton;
-
-    private View.OnClickListener startButtonListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            Intent i = new Intent(getApplicationContext(), GameActivity.class);
-            startActivity(i);
-        }
-    };
-
+public class GameActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button startButton = (Button) findViewById(R.id.b_start_game);
-        startButton.setOnClickListener(startButtonListener);
+        setContentView(R.layout.activity_game);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_game, menu);
         return true;
     }
 
